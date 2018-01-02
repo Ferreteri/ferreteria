eef42bcc9a20a2b38d650a03e95ab362047ad591
hpere <hpere@DESKTOP-G39TU04>
2018-01-02 17:03:36 -0600

    Revert "cc"
    This reverts commit a7e647607a51049a8abc17787c682e090164930f.

--- Ferreteria/src/Vista/Principal/Ventas.java
+++ Ferreteria/src/Vista/Principal/Ventas.java
@@ -27,91 +27,21 @@ public class Ventas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jSpinner1 = new javax.swing.JSpinner();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
-        jFormattedTextField1 = new javax.swing.JFormattedTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jFormattedTextField2 = new javax.swing.JFormattedTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jButton3 = new javax.swing.JButton();
-        jToggleButton1 = new javax.swing.JToggleButton();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 3, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(204, 255, 204));
-        jLabel1.setText("Bienvenido de nuevo ");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(260, 40, 193, 22);
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(203, 100, 290, 240);
-        getContentPane().add(jSpinner1);
-        jSpinner1.setBounds(240, 560, 40, 32);
-
-        jButton1.setFont(new java.awt.Font("Tahoma", 3, 18)); // NOI18N
-        jButton1.setText(">");
-        getContentPane().add(jButton1);
-        jButton1.setBounds(510, 190, 47, 31);
-
-        jButton2.setFont(new java.awt.Font("Tahoma", 3, 18)); // NOI18N
-        jButton2.setText("<");
-        getContentPane().add(jButton2);
-        jButton2.setBounds(110, 200, 47, 31);
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        jLabel3.setText("Cantidad de articulos por vender");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(0, 560, 234, 30);
-        getContentPane().add(jFormattedTextField1);
-        jFormattedTextField1.setBounds(40, 490, 131, 35);
-
-        jLabel4.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        jLabel4.setText("Piezas existentes en el inventario");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(20, 470, 233, 17);
-        getContentPane().add(jFormattedTextField2);
-        jFormattedTextField2.setBounds(250, 500, 310, 30);
-
-        jLabel5.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        jLabel5.setText("Descripcion del articulo ");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(300, 470, 161, 17);
-
-        jButton3.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        jButton3.setText("vender");
-        getContentPane().add(jButton3);
-        jButton3.setBounds(410, 560, 94, 33);
-
-        jToggleButton1.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
-        jToggleButton1.setText("Cancelar");
-        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jToggleButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jToggleButton1);
-        jToggleButton1.setBounds(580, 560, 91, 30);
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(-4, 40, 960, 0);
-
-        jLabel7.setIcon(new javax.swing.ImageIcon("C:\\Users\\hpere\\Desktop\\logo.png")); // NOI18N
-        getContentPane().add(jLabel7);
-        jLabel7.setBounds(0, 0, 730, 600);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -149,19 +79,5 @@ public class Ventas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JFormattedTextField jFormattedTextField1;
-    private javax.swing.JFormattedTextField jFormattedTextField2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JSpinner jSpinner1;
-    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
